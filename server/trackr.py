c3afd7b4c9d9d8901572d74400d2a5ddc8ae369a
Ben Carothers <benjamin.carothers13@ncf.edu>
2015-12-23 13:34:33 -0500

    The server can consume our api now

--- server/trackr.py
+++ server/trackr.py
@@ -1,11 +1,24 @@
 import flask
+import requests
 from flask import Flask
 
 app = Flask(__name__)
+task = {"do you": "work"}
+
 
 @app.route("/")
 def index():
     return flask.render_template('index.html')
+
+
+@app.route("/api_test")
+def create_task():
+    r = requests.get("http://127.0.0.1:8000/User")
+    if r.status_code != 200:
+        return
+    return r._content
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
