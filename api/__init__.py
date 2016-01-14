6b2e5f181b28a9148102c5369eadc93f8d46a75c
SJCaldwell <shane.caldwell12@ncf.edu>
2016-01-14 18:02:27 -0500

    Adding stuff to the API to work with Flasks login

--- api/__init__.py
+++ api/__init__.py
@@ -35,6 +35,35 @@ class User(db.Document):
     def __unicode__(self):
         return self.user_id
 
+    @property 
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True 
+
+    @property
+    def is_anonymous(self):
+        return False 
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except AttributeError:
+            raise NotImplementedError("No `id` attribute.")
+
+    def __eq__(self, other):
+        if isinstance(other, userMixin):
+            return self.get_id() == other.get_id()
+        return NotImplemented
+
+    def __ne__(self, other):
+        equal = self.__eq__(other)
+        if equal is NotImplemented:
+            return NotImplemented
+        return not equal
+
 class getUser(restful.Resource):
     def get(self, user_id = None):
         data = request.get_json()
