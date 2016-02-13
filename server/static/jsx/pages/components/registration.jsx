4b71e5738ed1f2bd24eb01e47d1391e3cd5e84fa
Ben Carothers <benjamin.carothers13@ncf.edu>
2016-02-13 13:55:02 -0500

    back online

--- server/static/jsx/pages/components/registration.jsx
+++ server/static/jsx/pages/components/registration.jsx
@@ -11,19 +11,19 @@ var Registration = React.createClass({
     getInitialState: function () {
         return {
             step: 1,
-        fieldValues = {
-            username: null,
-            email: null,
-            password: null
-        }
         }
     },
 
+    fieldValues: {
+      username: null,
+      email: null,
+      password: null
+    },
 
     saveValues: function (data) {
-        this.state.fieldValues.username = data.username 
-        this.state.fieldValues.email = data.email
-        this.state.fieldValue.password = data.password 
+        this.fieldValues.username = data.username 
+        this.fieldValues.email = data.email
+        this.fieldValues.password = data.password
     },
 
     nextStep: function () {
@@ -60,8 +60,8 @@ var Registration = React.createClass({
 
 
     submitRegistration: function () {
-        var user_id = this.state.fieldValues.username
-        var email = this.state.fieldValues.email
+        var user_id = this.fieldValues.username
+        var email = this.fieldValues.email
         var failureReason = "Value of user_id is " + user_id;
         console.log(failureReason)
         var Url = "/api_check/" + user_id + "/" + email;
@@ -80,7 +80,7 @@ var Registration = React.createClass({
     },
 
     registerUser: function () {
-        var Url = "/api_post/" + this.state.fieldValues.username + "/" + this.state.fieldValues.password + "/" + this.state.fieldValues.email + "/";
+        var Url = "/api_post/" + this.fieldValues.username + "/" + this.fieldValues.password + "/" + this.fieldValues.email + "/";
         var response = null
         jQuery.ajax({
             async: false,
@@ -100,17 +100,17 @@ var Registration = React.createClass({
     showStep: function () {
         switch (this.state.step) {
             case 1:
-                return <AccountFields fieldValues={this.state.fieldValues}
+                return <AccountFields fieldValues={this.fieldValues}
                                       nextStep={this.nextStep}
                                       hideRegister={this.props.hideRegister}
                                       closeModal={this.closeModal}
                                       saveValues={this.saveValues}
                                       submitRegistration={this.submitRegistration}/>
             case 2:
-                return <Failure fieldValues={this.state.fieldValues}
+                return <Failure fieldValues={this.fieldValues}
                                 previousStep={this.previousStep}/>
             case 3:
-                return <Success fieldValues={this.state.fieldValues}/>
+                return <Success fieldValues={this.fieldValues}/>
 
         }
     },
