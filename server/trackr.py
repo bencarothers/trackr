ef38c327b2446ea3e74ade34c0c6a91d221ad814
SJCaldwell <shane.caldwell12@ncf.edu>
2016-02-18 22:26:41 -0500

    Closing out card 42 - File storage for trackr
    *Added enviormental variables, and added the skeleton of a new
    path route that will access AWS from flask. You can find this
    at trackr.py

--- server/trackr.py
+++ server/trackr.py
@@ -132,5 +132,30 @@ def delete_user(username):
         return "IMPROPER"
     return r._content
 
+@app.route('/sign_s3/')
+def sign_s3():
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    object_name = urllib.quote_plus(request.args.get('file_name'))
+    mime_type = request.args.get('file_type')
+
+    expires = int(time.time()+60*60*24)
+    amz_headers = "x-amz-acl:public-read"
+
+    string_to_sign = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY.encode(), string_to_sign.encode('utf8'), sha1).digest())
+    signature = urllib.quote_plus(signature.strip())
+
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+
+    content = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+        'url': url,
+    })
+    return content
+
 if __name__ == "__main__":
     app.run(debug = True)
