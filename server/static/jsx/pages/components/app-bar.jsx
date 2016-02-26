34b0c81b7991420f77f1a06225028cbc257aa995
Ben Carothers <benjamin.carothers13@ncf.edu>
2016-02-25 20:44:48 -0500

    I swear I pushed this

--- server/static/jsx/pages/components/app-bar.jsx
+++ server/static/jsx/pages/components/app-bar.jsx
@@ -9,18 +9,15 @@ const MainAppBar = React.createClass({
     },
 
     handleSubmit(){
-        var formData = {
-            lift: React.findDOMNode(this.refs.lift).value,
-            weight: React.findDOMNode(this.refs.weight).value,
-            video: React.findDOMNode(this.refs.video).value
-        };
-        this.props.handleResponse(formData)
+        var fd = new FormData()
+        fd.append( 'file', this.refs.file.files[0] );
+        this.props.handleResponse(fd)
         jQuery.ajax({
-            url: '/ajaxVideoUpload/' + formData.lift + '/' + formData.weight + '/',
-            type: 'POST',
-            data: formData,
-            cache: false,
+            url: '/ajaxVideoUpload/' + this.refs.lift + '/' + this.refs.weight + '/',
+            data: fd,
             processData: false,
+            contentType: false,
+            type: 'POST',
             success: function (data) {
                 alert('Success');
             }
@@ -29,7 +26,7 @@ const MainAppBar = React.createClass({
     },
 
     intro(){
-      introJs().start();
+        introJs().start();
     },
 
     logOut(){
@@ -42,6 +39,7 @@ const MainAppBar = React.createClass({
 
     render() {
         var divStyle = {
+            background: 'lightgrey',
             padding: '2em',
             textAlign: 'center'
         }
@@ -51,43 +49,60 @@ const MainAppBar = React.createClass({
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <a className="navbar-brand brand-image" href="#" data-step="3" data-intro="We'll handle the rest.">
+                            <a className="navbar-brand brand-image" href="#" data-step="3"
+                               data-intro="We'll handle the rest.">
                                 <img alt="Brand" src="./static/img/bar.png"></img>
                             </a>
                             <div className="navbar-brand">
                                 <p>Trackr</p>
                             </div>
                         </div>
-                        <btn className="btn btn-success navbar-btn pull-right margin-right" onClick={this.addLift}data-step="1" data-intro="Click here to add a lift!">Add</btn>
-                        <btn className="btn btn-warning navbar-btn pull-right margin-right" onClick={this.logOut}>Logout</btn>
-                        <btn className="btn btn-info navbar-btn pull-right margin-right" onClick={this.intro}>Help?</btn>
+                        <btn className="btn btn-success navbar-btn pull-right margin-right" onClick={this.addLift}
+                             data-step="1" data-intro="Click here to add a lift!">Add
+                        </btn>
+                        <btn className="btn btn-warning navbar-btn pull-right margin-right" onClick={this.logOut}>
+                            Logout
+                        </btn>
+                        <btn className="btn btn-info navbar-btn pull-right margin-right" onClick={this.intro}>Help?
+                        </btn>
 
                         <Modal ref="modal">
-                              <div style={divStyle}>
-                                  <form>
-                                      <div className="form-group row">
-                                          <h2 className="col-md-12 text-center">Add a Lift</h2>
-                                      </div>
-                                      <div className="form-group row">
-                                          Lift: <input type="text" ref="lift"></input><br></br>
-                                      </div>
-                                      <div className="form-group row">
-                                          Weight: <input type="text" ref="weight"></input><br></br>
-                                      </div>
-                                      <div className="form-group row">
-                                        <span className="btn btn-default btn-file">
-                                            Browse: <input type="file" ref="video" accept="image/*"></input>
-                                        </span>
-                                      </div>
-                                      <div className="form-group row">
-                                          <button type='button' className="btn btn-secondary col-md-offset-3 col-md-6"
-                                                  onClick={this.handleSubmit}>Add
-                                          </button>
-                                      </div>
-                                  </form>
-                              </div>
-                          </Modal>
-                     </div>
+                            <div style={divStyle}>
+                                <form className="form-horizontal" role="form">
+                                    <div className="form-group row text-center">
+                                        <img id='biceps' src='../static/img/Flex.png'></img>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label"
+                                               htmlFor="lift">Lift</label>
+                                        <div className="col-sm-10">
+                                            <input ref='lift' className="form-control"
+                                                   id="lift" placeholder="Lift"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label"
+                                               htmlFor="weight">Weight</label>
+                                        <div className="col-sm-10">
+                                            <input ref='weight' className="form-control"
+                                                   id="weight" placeholder="Weight"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label"
+                                               htmlFor="weight">Weight</label>
+                                        <input ref='file' id="file" type="file" className="btn-file"></input>
+                                    </div>
+                                    <div className="form-group row">
+                                        <button type='button' className="btn btn-success col-md-offset-3 col-md-6"
+                                                onClick={this.handleSubmit}>Add
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </Modal>
+                    </div>
                 </nav>
             </div>
         );
