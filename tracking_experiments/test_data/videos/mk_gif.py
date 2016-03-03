6bd217aeaed5ef9babcd292ac6dad33f44fde7c6
Ben Carothers <benjamin.carothers13@ncf.edu>
2016-03-03 18:05:58 -0500

    a bit smaller gifs

--- tracking_experiments/test_data/videos/mk_gif.py
+++ tracking_experiments/test_data/videos/mk_gif.py
@@ -1,3 +1,3 @@
 from moviepy.editor import *
-clip = VideoFileClip("deadlift.mp4")
-clip.write_gif("vid.gif")
+clip = VideoFileClip("bench.mp4").subclip((22.65),(41)).resize(.25)
+clip.write_gif("test.gif", fps=12,fuzz=30)
