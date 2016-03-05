653fe13e09e504866589893cb4bdc5ee8136bbda
Ben Carothers <benjamin.carothers13@ncf.edu>
2016-03-05 14:09:18 -0500

    It seems like cv is imported in trackr.py properly

--- tracking_experiments/test_data/videos/mk_gif.py
+++ tracking_experiments/test_data/videos/mk_gif.py
@@ -1,3 +1,3 @@
 from moviepy.editor import *
-clip = VideoFileClip("bench.mp4").subclip((22.65),(41)).resize(.25)
-clip.write_gif("test.gif", fps=12,fuzz=30)
+clip = VideoFileClip("bench.mp4").subclip((32),(42)).resize(width=240)
+clip.write_gif("test2.gif", fps=1)
